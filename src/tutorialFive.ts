618947db1e230f4b12f514521d8c20a2a51aa99d
Abhishek Tiwari <tiwariabhishek27611@gmail.com>
2024-04-02 01:59:58 +0530

    learnt generic and their use case with variables function interface and created some gf

--- src/tutorialFive.ts
+++ src/tutorialFive.ts
@@ -1,4 +1,45 @@
-//const abhishek ="abhishek"
+type valueType = string | number | boolean
+
+let value : ValueType
+const random = Math.random()
+value = random<0.33 ?  'hello' : random<0.66 ? 123.456 :true;
+
+//console.log(value)
+
+function checkValue(value : ValueType):void{
+    if(typeof value === "string"){
+        console.log(value.toUpperCase())
+    }else if(typeof value === "number"){
+        console.log(value.toFixed(2))
+    }else{
+        console.log(value?"True":"False");
+    }
+}
+
+checkValue(value)
 
 
-let thanon = 'sjsjs'
+//equality narrowing.....................................
+
+type Dog = {name: string,type: string,bark:()=>void,}
+type Cat = {name: string , type: "cat" ,claw: () => void}
+type Animall = Dog | Cat
+
+// function checkAnimalSoundType(animal: Animal){
+//     if(animal.type==='dog'){
+//     animal.bark()
+//     }else{
+//         animal.claw();
+//     }
+// }
+
+// function checkAnimalSoundType(animal: Animall){
+//     if('bark' in animal){
+//       animal.bark();
+//     }else{
+//         animal.claw();
+//     }
+// }
+
+
+//truthy and falsy gaurd................................................................
